6ad9e6007de15ac43dca04af24da1d7dfed61ba9
Cinthia <chuertas@fi.uba.ar>
2023-11-07 00:15:46 -0300

    segunda modificacion js

--- Js/compra-tickets.js
+++ Js/compra-tickets.js
@@ -1,4 +1,20 @@
 
+// Agregar un event listener al botón "Resumen" para llamar a la función calcularTotal
+const btnResumen = document.getElementById('btnResumen');
+btnResumen.addEventListener('click', calcularTotal);
+
+// Defino porcentajes de descuento segÃºn categorÃ­a
+let descuentoEstudiante = 80;
+let descuentoTrainee    = 50;
+let descuentoJunior     = 15;
+
+// Elementos en variables
+let nombre          = document.getElementById("nombre");
+let apellido        = document.getElementById("apellido");
+let mail            = document.getElementById("mail");
+let cantidadTickets = document.getElementById("cantidadTickets");
+let categoria       = document.getElementById("categoriaSelect");
+
 // Función para calcular el total y mostrarlo
 function calcularTotal() {
 
