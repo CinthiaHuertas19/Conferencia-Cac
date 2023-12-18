5792aee2e2083fc4b4bb3a8efd6de80bfe0982d4
Cinthia <chuertas@fi.uba.ar>
2023-12-18 14:24:18 -0300

    Modificando codigo js

--- Js/compra-tickets.js
+++ Js/compra-tickets.js
@@ -1,3 +1,14 @@
+// Obtener referencia al botón "Borrar"
+const btnBorrar = document.getElementById('btnBorrar');
+
+// Agregar un event listener al botón "Borrar" para llamar a la función borrarTotal
+btnBorrar.addEventListener('click', borrarTotal);
+
+// Función para borrar el contenido de "Total a pagar"
+function borrarTotal() {
+    const totalPago = document.getElementById('totalPago');
+    totalPago.innerHTML = ''; 
+}
 
 // Agregar un event listener al botón "Resumen" para llamar a la función calcularTotal
 const btnResumen = document.getElementById('btnResumen');
@@ -16,20 +27,28 @@ function calcularTotal() {
 
     let precio = 200;
 
-    if(categoriaSelect == "-- Seleccione --"){
-        //el mismo precio
-        precio = 200;
-    }else if(categoriaSelect == "estudiante"){
-        //Estudiante 80%
-        precio = 200 - (200*0.80);
-    
-    }else if (categoriaSelect == "trainee") {
-        //Trainee 50%
-        precio = 200 - (200*0.50)
-    }else{
-        //Junior 15 %
-        precio = 200 - (200 *0.15);
-    }    
+    switch (categoriaSelect) {
+        case "-- Seleccione --":
+            // el mismo precio
+            precio = 200;
+            break;
+        case "estudiante":
+            // Estudiante 80%
+            precio = 200 - (200 * 0.80);
+            break;
+        case "trainee":
+            // Trainee 50%
+            precio = 200 - (200 * 0.50);
+            break;
+        case "junior":
+            // Junior 15%
+            precio = 200 - (200 * 0.15);
+            break;
+        default:
+            // el mismo precio por defecto
+            precio = 200;
+            break;
+    }
 
     // Mostrar el total en la página
     totalPago.textContent = precio;
